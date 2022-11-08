84f6f24c26f2323a749213e8ff1b7d312aabc9aa
N1mmu <nimanthran@gmail.com>
2022-11-08 19:15:43 +0530

    sprint 2

---
+++ Project Development Phase/Sprint 2/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from PIL import Image
+import numpy as np
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+import os
+
+y_pred = 0
+app = Flask(__name__)
+model = load_model('E:\Github\IBM-Project-43092-1660712903\Project Development Phase\Sprint 2\models\mnistCNN.h5')
+
+@app.route('/')
+def upload_file():
+    return render_template('index.html')
+
+@app.route('/home')
+def upload_file1():
+    return render_template('index.html')
+
+@app.route('/about')
+def upload_file2():
+    return render_template('index.html')
+
+@app.route('/upload')
+def upload_file3():
+    return render_template('web.html')
+
+@app.route('/recognize')
+def upload_file4():
+    return render_template('web.html')
+
+@app.route('/predict', methods =['POST'])
+def upload_image_file():
+    if request.method == 'POST':
+        img = Image.open(request.files['file'].stream).convert("L")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8000,debug=True)
