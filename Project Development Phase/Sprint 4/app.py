0dcb1d9252fe683623c04a8d505bc7c03b92e5b6
N1mmu <nimanthran@gmail.com>
2022-11-08 19:29:33 +0530

    sprint 4

---
+++ Project Development Phase/Sprint 4/app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request
+from PIL import Image
+import numpy as np
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+import os
+
+y_pred = 0
+app = Flask(__name__)
+model = load_model('E:\Github\IBM-Project-43092-1660712903\Project Development Phase\Sprint 2\models\mnistCNN.h5')
+
+@app.route('/')
+def upload_file():
+    return render_template('index.html')
+
+@app.route('/home')
+def upload_file1():
+    return render_template('index.html')
+
+@app.route('/about')
+def upload_file2():
+    return render_template('index.html')
+
+@app.route('/upload')
+def upload_file3():
+    return render_template('web.html')
+
+@app.route('/recognize')
+def upload_file4():
+    return render_template('web.html')
+
+@app.route('/predict', methods =['POST'])
+def upload_image_file():
+    if request.method == 'POST':
+        img = Image.open(request.files['file'].stream).convert("L")
+        img = img.resize((28,28))
+        im2arr = np.array(img)
+        im2arr = im2arr.reshape(1,28,28,1)
+        y_pred = np.argmax(model.predict(im2arr), axis=-1)
+        print(y_pred)
+        if (y_pred==0):
+            return render_template("output.html", showcase = str(y_pred))
+        elif (y_pred==1):
+            return render_template("output.html" , showcase = str(y_pred))
+        elif (y_pred==2):
+            return render_template("output.html" , showcase = str(y_pred))
+        elif (y_pred==3):
+            return render_template("output.html" , showcase = str(y_pred))
+        elif (y_pred==4):
+            return render_template("output.html" , showcase = str(y_pred))
+        elif (y_pred==5):
+            return render_template("output.html" , showcase = str(y_pred))
+        elif (y_pred==6):
+            return render_template("output.html" , showcase = str(y_pred))
+        elif (y_pred==7):
+            return render_template("output.html" , showcase = str(y_pred))
+        elif (y_pred==8):
+            return render_template("output.html" , showcase = str(y_pred))
+        elif (y_pred==9):
+            return render_template("output.html" , showcase = str(y_pred))
+        else:
+            return None
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8000,debug=True)
